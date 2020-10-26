2af7ba5ab3c9c60da34dd078b6326bb37e9ea989
cantataMajora <fufulomoffu@gmail.com>
2020-10-26 19:26:08 +0300

    getting first 500 pages in namespace

--- script.py
+++ script.py
@@ -7,8 +7,15 @@ SITENAME: str = 'https://en.wikipedia.org/'
 def get_data():
 
     session = mwapi.Session(SITENAME, user_agent="LostEnchanter")
-    print(session.get(action='query', meta='userinfo'))
+    params = {'action': 'query',
+              'format': 'json',
+              'list': 'allpages',
+              'apnamespace': '828',    # https://en.wikipedia.org/wiki/Special:PrefixIndex?prefix=&namespace=828
+              'aplimit': 'max',        # letting the request get first 500 pages
+              'maxlag': '3'}           # waiting about 3 seconds is ok if needed
 
+    request_data = session.get(params)
+    print('Done')
 
 if __name__ == "__main__":
     get_data()
